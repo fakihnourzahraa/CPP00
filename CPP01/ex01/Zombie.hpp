d391d9dd6963d47087619eb09d4d081d7f1083a6
Nour Al Zahraa Fakih <nfakih@c2r4s2.42beirut.com>
2025-12-29 15:22:39 +0200

    cpp01 ex01 done

---
+++ CPP01/ex01/Zombie.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfakih <nfakih@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/28 20:28:04 by nour              #+#    #+#             */
+/*   Updated: 2025/12/29 15:11:43 by nfakih           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include "fstream"
+#include "iomanip"
+#include "cstdlib"
+
+class Zombie {
+    private: 
+    std::string name;
+
+    public:
+	~Zombie();
+	void		set_name(std::string name);
+    void		announce(void);
+};
+
+Zombie	*zombieHorde(int N, std::string name);
+
+#endif
--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,15 +6,20 @@
 /*   By: nfakih <nfakih@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 13:47:46 by nfakih            #+#    #+#             */
-/*   Updated: 2025/12/29 15:03:24 by nfakih           ###   ########.fr       */
+/*   Updated: 2025/12/29 15:21:32 by nfakih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
 int main(void)
 {
-	Zombie *zom = zombieHorde(2, "nour");
+	int	i = 5;
+	Zombie *zom = zombieHorde(5, "nour");
+	for (int j = 0; j < i; j++)
+	{
+		zom[j].announce();
+	}
+	delete[] zom;
     return (0);
 }
