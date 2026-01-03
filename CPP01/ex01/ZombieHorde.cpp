5177c3131f938ee85cc8b520a70fc9c7c3208801
Nour Fakih <fakihnourzahraa@gmail.com>
2026-01-03 13:28:17 +0200

    ex01 main

--- CPP01/ex01/ZombieHorde.cpp
+++ CPP01/ex01/ZombieHorde.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   ZombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nour <nour@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 14:25:41 by nour              #+#    #+#             */
-/*   Updated: 2025/12/30 14:25:54 by nour             ###   ########.fr       */
+/*   Updated: 2026/01/03 13:24:13 by nour             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,21 @@ Zombie	*zombieHorde(int N, std::string name)
 	}
 	return (zombie);
 }
+
+// Zombie	*zombieHorde(int N, std::string name)
+// {
+// 	int		i;
+//     Zombie *zombie = new Zombie[N];
+
+// 	i = 1;
+// 	zombie[0].set_name("mariam");
+// 	while (i < N)
+// 	{
+// 		if (i %2 == 0)
+// 			zombie[i].set_name(name);
+// 		else
+// 			zombie[i].set_name(name + "X");
+// 		i++;
+// 	}
+// 	return (zombie);
+// }
